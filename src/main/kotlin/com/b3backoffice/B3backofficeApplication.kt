d01bc5b56b413968b5421d241748d3c9361d26ff
rugii913 <rugii913@gmail.com>
2024-01-22 11:44:11 +0900

    프로젝트 생성, 빌드 파일들

---
+++ src/main/kotlin/com/b3backoffice/B3backofficeApplication.kt
@@ -0,0 +1,11 @@
+package com.b3backoffice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class B3backofficeApplication
+
+fun main(args: Array<String>) {
+    runApplication<B3backofficeApplication>(*args)
+}
