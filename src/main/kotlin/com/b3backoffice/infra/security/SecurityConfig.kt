b3da6ae0b195c512f4bc17f8a131960cdf0643cf
rugii913 <rugii913@gmail.com>
2024-01-22 22:18:24 +0900

    feature/project-init에서 누락된 부분 수정-플러그인 종속성, 필요 없는 import 제거
    feature/project-init에서 누락된 부분 수정
    - 플러그인 종속성 추가
      - JPA를 사용하기 위해 noArg, allOpen 플러그인 적용
    - 필요 없는 import 제거
      - SecurityConfig 필요 없는 import 제거

--- src/main/kotlin/com/b3backoffice/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/b3backoffice/infra/security/SecurityConfig.kt
@@ -1,12 +1,10 @@
 package com.b3backoffice.infra.security
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector
 
