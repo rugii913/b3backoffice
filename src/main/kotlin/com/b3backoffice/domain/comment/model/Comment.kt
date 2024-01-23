02c91b34b7468052f2e6877be933dd7f639fa604
rugii913 <rugii913@gmail.com>
2024-01-23 10:32:41 +0900

    Comment의 Base Entity 상속 누락된 부분 추가
    Comment의 Base Entity 상속 누락된 부분 추가

--- src/main/kotlin/com/b3backoffice/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/b3backoffice/domain/comment/model/Comment.kt
@@ -1,6 +1,7 @@
 package com.b3backoffice.domain.comment.model
 
 import com.b3backoffice.domain.review.model.Review
+import com.b3backoffice.domain.shared.model.BaseEntity
 import com.b3backoffice.domain.user.model.User
 import jakarta.persistence.*
 import java.time.LocalDateTime
@@ -10,7 +11,7 @@ class Comment(
     @ManyToOne @JoinColumn(name = "user_id") val user: User,
     @ManyToOne @JoinColumn(name = "review_id") val review: Review,
     var content: String,
-) {
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
