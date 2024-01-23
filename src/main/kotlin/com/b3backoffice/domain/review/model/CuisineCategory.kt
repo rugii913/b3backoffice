5247ef4770b9e6871e5d6fc2f9ade843371eec48
rugii913 <rugii913@gmail.com>
2024-01-23 17:01:10 +0900

    요청 JSON body의 cuisineCategory String을 CuisinCategory enum으로 매핑
    - 요청 JSON body의 cuisineCategory String을 CuisinCategory enum으로 매핑하여 가져옴
      - @JsonCreator 사용
        - JSON string을 알아서 enum으로 바꿔주게 함
      - static처럼 사용하기 위해 @JvmStatic 사용

--- src/main/kotlin/com/b3backoffice/domain/review/model/CuisineCategory.kt
+++ src/main/kotlin/com/b3backoffice/domain/review/model/CuisineCategory.kt
@@ -1,5 +1,8 @@
 package com.b3backoffice.domain.review.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import org.apache.commons.lang3.EnumUtils
+
 enum class CuisineCategory(val categoryInKorean: String) {
     KOREAN("한식"),
     BUNSIK("분식"),
@@ -9,4 +12,12 @@ enum class CuisineCategory(val categoryInKorean: String) {
     ASIAN("아시아 음식"),
     PUB("술집"),
     CAFE_AND_DESSERT("카페, 디저트");
+
+    companion object {
+        @JvmStatic
+        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+        fun parse(name: String?): CuisineCategory =
+            name?.let { EnumUtils.getEnumIgnoreCase(CuisineCategory::class.java, it.trim()) }
+                ?: throw IllegalArgumentException("해당하는 음식 카테고리가 없습니다.") // TODO 예외 메시지
+    }
 }
