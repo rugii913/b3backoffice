df17eb72535628d0df9ff002dd31aa3b39c05780
siadeu87 <woxingya098@gmail.com>
2024-01-23 16:35:58 +0900

    login 구현

--- src/main/kotlin/com/b3backoffice/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/b3backoffice/domain/user/controller/UserController.kt
@@ -1,5 +1,7 @@
 package com.b3backoffice.domain.user.controller
 
+import com.b3backoffice.domain.user.dto.LoginArgument
+import com.b3backoffice.domain.user.dto.LoginDto
 import com.b3backoffice.domain.user.dto.SignupArgument
 import com.b3backoffice.domain.user.dto.UserDto
 import com.b3backoffice.domain.user.service.UserService
@@ -18,5 +20,11 @@ class UserController(
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userService.signup(signupArgument))
+    }
+    @PostMapping("/login")
+    fun login(@RequestBody loginArgument: LoginArgument): ResponseEntity<LoginDto>{
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.login(loginArgument))
     }
 }
