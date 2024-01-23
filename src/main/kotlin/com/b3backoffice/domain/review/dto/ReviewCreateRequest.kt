851ddc78dac6588ec979fc172e0f429fac0b3d89
rugii913 <rugii913@gmail.com>
2024-01-23 14:10:01 +0900

    현재 ReviewController에서 필요한 DTO 작성
    - 현재 ReviewController에서 필요한 DTO 작성
      - ReviewCreateRequest
      - ReviewUpdateRequest
      - ReviewResponse
    
    - ReviewCreateRequest와 ReviewUpdateRequest가 현재 내용이 같으나 추후 어떻게 변경이 될지 모르므로 따로 나눠놓았음

--- src/main/kotlin/com/b3backoffice/domain/review/dto/ReviewCreateRequest.kt
+++ src/main/kotlin/com/b3backoffice/domain/review/dto/ReviewCreateRequest.kt
@@ -1,3 +1,15 @@
 package com.b3backoffice.domain.review.dto
 
-class ReviewCreateRequest
+import com.b3backoffice.domain.review.model.CuisineCategory
+import java.time.LocalDateTime
+
+data class ReviewCreateRequest(
+    val storeName: String,
+    val cuisineCategory: CuisineCategory,
+    val address: String,
+    val visitedAt: LocalDateTime,
+    val waitingTime: Int,
+    val storeSize: Int,
+    val cleanliness: Int,
+    val content: String,
+)
--- src/main/kotlin/com/b3backoffice/domain/review/dto/ReviewResponse.kt
+++ src/main/kotlin/com/b3backoffice/domain/review/dto/ReviewResponse.kt
@@ -1,3 +1,18 @@
 package com.b3backoffice.domain.review.dto
 
-class ReviewResponse
+import com.b3backoffice.domain.review.model.CuisineCategory
+import java.time.LocalDateTime
+
+data class ReviewResponse(
+    val createdAt: LocalDateTime,
+    val lastModifiedAt: LocalDateTime,
+    val userNickname: String,
+    val storeName: String,
+    val cuisineCategory: CuisineCategory,
+    val address: String,
+    val visitedAt: LocalDateTime,
+    val waitingTime: Int,
+    val storeSize: Int,
+    val cleanliness: Int,
+    val content: String,
+)
