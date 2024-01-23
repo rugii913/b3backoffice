1f00f554557c0825f80bbd4aa451d09ccdbe2f12
rugii913 <rugii913@gmail.com>
2024-01-23 14:27:26 +0900

    ReviewController 어노테이션 및 URI 매핑 잘못된 부분 발견하여 수정
    - ReviewController 어노테이션 및 URI 매핑 잘못된 부분 발견하여 수정

--- src/main/kotlin/com/b3backoffice/domain/review/controller/ReviewController.kt
+++ src/main/kotlin/com/b3backoffice/domain/review/controller/ReviewController.kt
@@ -5,15 +5,10 @@ import com.b3backoffice.domain.review.dto.ReviewResponse
 import com.b3backoffice.domain.review.dto.ReviewUpdateRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController("reviews")
+@RequestMapping("/reviews")
+@RestController
 class ReviewController {
 
     @GetMapping("/{reviewId}")
