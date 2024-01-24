8e298f7268a681395d08ede04bf1081f86344090
siadeu87 <woxingya098@gmail.com>
2024-01-24 11:08:55 +0900

    JWT 인증구현-JwtAuthenticationFilter 적용누락

--- src/main/kotlin/com/b3backoffice/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/b3backoffice/infra/security/SecurityConfig.kt
@@ -1,16 +1,20 @@
 package com.b3backoffice.infra.security
 
+import com.b3backoffice.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
 
     @Bean
     fun filterChain(httpSecurity: HttpSecurity, introspector: HandlerMappingIntrospector): SecurityFilterChain {
@@ -21,13 +25,14 @@ class SecurityConfig {
             .headers { it.frameOptions { frameOptionsConfig -> frameOptionsConfig.sameOrigin() } } // 콘솔 H2 사용 위해서 필요
             .authorizeHttpRequests {
                 it.requestMatchers(
-                    "/**", // 개발 초기 단계 - 우선 모든 요청에 대해 인증 없이 열어놓음
+                    "/**",// 개발 초기 단계 - 우선 모든 요청에 대해 인증 없이 열어놓음
                 ).permitAll()
                     .requestMatchers( // https://colabear754.tistory.com/170 - Spring Security 사용하면서 H2 콘솔 사용
                         MvcRequestMatcher(introspector, "/**").apply { setServletPath("/h2-console") }
                     ).permitAll()
                     .anyRequest().authenticated()
             }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
     }
 }
