77e862c129e559b06b776b33a3f86e3975f646db
t2023-m0027 <y974348@gmail.com>
2024-01-25 13:29:24 +0900

    글자수 제한코드 작성

--- src/main/kotlin/com/b3backoffice/domain/notice/dto/CreateNoticeRequest.kt
+++ src/main/kotlin/com/b3backoffice/domain/notice/dto/CreateNoticeRequest.kt
@@ -1,6 +1,16 @@
 package com.b3backoffice.domain.notice.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class CreateNoticeRequest(
+    @field:NotBlank(message = "내용을 작성해주세요.")
+    @field:Size(min = 1, max = 50, message = "글자수는 50자를 초과할 수 없습니다.")
     val title: String,
+
+    @field:NotBlank(message = "내용을 작성해주세요.")
+    @field:Size(min = 1, max = 1000, message = "글자수는 1000자를 초과할 수 없습니다.")
     val content: String,
+
 )
+
