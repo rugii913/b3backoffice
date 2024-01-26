b3c874cd372c1a37348db700c992b41d87cc36f5
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-01-26 20:35:05 +0900

    test code

--- src/test/kotlin/com/b3backoffice/domain/comment/controller/CommentControllerTest.kt
+++ src/test/kotlin/com/b3backoffice/domain/comment/controller/CommentControllerTest.kt
@@ -1,7 +1,9 @@
 package com.b3backoffice.domain.comment.controller
 
+import com.b3backoffice.domain.comment.dto.CommentRequest
 import com.b3backoffice.domain.comment.dto.CommentResponse
 import com.b3backoffice.domain.comment.service.CommentService
+import com.b3backoffice.infra.security.UserPrincipal
 import com.b3backoffice.infra.security.jwt.JwtPlugin
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -18,11 +20,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.domain.Page
+import org.springframework.http.MediaType
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.web.bind.annotation.RequestBody
 import java.time.LocalDateTime
 
 @SpringBootTest
@@ -34,47 +38,50 @@ class CommentControllerTest @Autowired constructor(
 ) : DescribeSpec({
     extension(SpringExtension)
 
+    // describe 끝날 때마다 설정했던 mocking 비워줌
     afterContainer {
         clearAllMocks()
     }
 
+    // CommentService mocking
     val commentService = mockk<CommentService>()
 
+    // COMMON - addComment
     describe("POST /reviews/{reviewId}/comments") {
-        context("존재하는 review에 존재하는 user가 댓글 생성을 요청할 때") {
+        context("댓글 생성을 요청할 때") {
             it("201 status code를 응답한다.") {
-                val userId = 1L
+
                 val reviewId = 1L
 
-                every { commentService.addComment(any(), any(), any()) } returns CommentResponse(
-                    id = 1,
-                    userId = userId,
+                // Mock 동작 정의
+                every { commentService.addComment(any(), reviewId, CommentRequest("test comment")) } returns CommentResponse(
+                    id = 1L,
+                    userId = 1L,
                     reviewId = reviewId,
-                    content = "test",
+                    content = "test comment",
                     createdAt = LocalDateTime.now(),
                     updatedAt = LocalDateTime.now(),
                     deletedAt = null
                 )
 
+                // access token 생성
                 val jwtToken = jwtPlugin.generateAccessToken(
                     subject = "1",
-                    username = "tester",
+                    username = "tester@gmail.com",
                     role = "COMMON"
                 )
 
+                // 요청 수행
                 val result = mockMvc.perform(
                     post("/reviews/$reviewId/comments")
                         .header("Authorization", "Bearer $jwtToken")
+                        .content("{\"content\": \"test comment\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
                 ).andReturn()
 
+                // status code 확인
                 result.response.status shouldBe 201
-
-                val responseDto = jacksonObjectMapper().readValue(
-                    result.response.contentAsString,
-                    CommentResponse::class.java
-                )
-
-                responseDto.reviewId shouldBe reviewId
             }
         }
     }
