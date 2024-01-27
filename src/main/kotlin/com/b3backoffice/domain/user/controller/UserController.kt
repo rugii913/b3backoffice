1ddb87957faac845d18dcf73b57b12e7faf92690
rugii913 <rugii913@gmail.com>
2024-01-28 01:40:22 +0900

    로그아웃 요청 시 refresh token도 받게 하여, access token과 함께 무효화 처리
    - 로그아웃 요청 시 refresh token도 받게 하여, access token과 함께 무효화 처리

--- src/main/kotlin/com/b3backoffice/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/b3backoffice/domain/user/controller/UserController.kt
@@ -36,9 +36,15 @@ class UserController(
     }
 
     @PostMapping("/logout")
-    fun logout(request: HttpServletRequest): ResponseEntity<Unit> {
-        val jwt = request.getBearerToken() ?: throw IllegalStateException("비정상 로그아웃 요청") // TODO 예외처리 다시
-        tokenInvalidationService.expireToken(jwt)
+    fun logout(request: HttpServletRequest, logoutArgument: LogoutArgument): ResponseEntity<Unit> {
+        request.getBearerToken()
+            ?.let { tokenInvalidationService.expireToken(it) } // access token 만료 처리
+            ?: throw IllegalStateException("비정상 로그아웃 요청") // TODO 예외처리 다시
+
+
+        logoutArgument.refreshToken
+            ?.let { tokenInvalidationService.expireToken(it) } // refresh token 만료 처리
+
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 
